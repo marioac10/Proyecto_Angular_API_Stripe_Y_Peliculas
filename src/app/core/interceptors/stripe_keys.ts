451163e58a4d174101bcd883c5baeb45c38480e7
marioac10 <77754107+marioac10@users.noreply.github.com>
2023-05-17 17:30:07 -0500

    Update stripe_keys.ts

--- src/app/core/interceptors/stripe_keys.ts
+++ src/app/core/interceptors/stripe_keys.ts
@@ -1,6 +1,6 @@
 
 
 export const STRIPE_KEYS:any = {
-    public: "pk_test_51K9Wd0FpmmHbZRLFwXU4vgPdWr1PFnalMZPjXLskPApr1GUslcVcgoeaGEbyKxCKMUT8XUIFXLe10zNlDu3ZWexL009cOJ3qIY",
-    secret: "sk_test_51K9Wd0FpmmHbZRLF60nagJyL0HFFnMbo3Ozvh0l1VdFC6qly2g6aXuliiNiMMHNlkMG2tY0JjHnQ5v0LdfPJ2Mxn0078JX0rAB"
+    public: "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%",
+    secret: "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 }
